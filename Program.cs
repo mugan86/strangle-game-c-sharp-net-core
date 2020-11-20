71bf191f98f725e5d8c7788bf47c5824d64bb11c
Anartz Mugika Ledo <mugan86@gmail.com>
2020-11-20 12:15:37 +0100

    1.- Empezamos con el menú principal

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StrangleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Menú 
+            Console.WriteLine("===============AHORCADO en C#===============");
+            Console.WriteLine("");
+            Console.WriteLine("Opciones del juego: ");
+            Console.WriteLine("1 ) Jugar partida aleatoria con todos los temas.");
+            Console.WriteLine("2 ) Jugar partida eligiendo tema.");
+            Console.WriteLine("3 ) Créditos del autor.");
+            Console.WriteLine("X ) Salir del juego.");
+            string optionSelect = Console.ReadLine();
+            switch (optionSelect) {
+                case "1":
+                    Console.WriteLine("Vamos a jugar partida aleatoria");
+                    break;
+                case "2": 
+                    Console.WriteLine("Vamos a jugar partida eligiendo tema");
+                    break;
+                case "3": 
+                    Console.WriteLine("Creador del juego - Anartz Mugika Ledo (mugan86@gmail.com)");
+                    Console.WriteLine("Curso \"Master en Programación C# con Visual Studio Code\"");
+                    Console.WriteLine("https://cursos.anartz-mugika.com/master-c-sharp-vsc");
+                    break;
+                case "X": 
+                case "x": 
+                    Console.WriteLine("¡¡Saliendo del juego!!");
+                    break;
+                default:
+                    optionSelect = "--";
+                    Console.WriteLine("Seleccione una de las opciones disponibles por favor.");
+                    break;
+            }
+        }
+    }
+}
